e4848555fadcb9394f4e31db7bdf80d27caae280
Milind Jamnekar <milindjamnekar502@gmail.com>
2024-02-12 20:25:50 +0530

    archived added

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -53,6 +53,7 @@ const Item = ({
   const { user } = useUser();
   const router = useRouter();
   const create = useMutation(api.documents.create);
+  const archive = useMutation(api.documents.archive);
 
   const handleExpand = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -77,6 +78,18 @@ const Item = ({
       loading: "Creating a new note...",
       success: "New note created!",
       error: "Failed to create a new note.",
+    });
+  };
+  const onArchive = async (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
+    if (!id) return;
+    const promise = archive({ id });
+    toast.promise(promise, {
+      loading: "Note moving to trash ...",
+      success: "Note moved to trash",
+      error: "Error on moving note to trash",
     });
   };
 
@@ -132,7 +145,7 @@ const Item = ({
               side="right"
               forceMount
             >
-              <DropdownMenuItem onClick={() => {}}>
+              <DropdownMenuItem onClick={onArchive}>
                 <Trash className="h-4 w-4 mr-2" />
                 Delete
               </DropdownMenuItem>
--- convex/documents.ts
+++ convex/documents.ts
@@ -1,5 +1,44 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { Id } from "./_generated/dataModel";
+
+const getUserIdentity = async (ctx: any): Promise<string> => {
+  const identity = await ctx.auth.getUserIdentity();
+  if (!identity) throw new Error("not authenticated");
+
+  const userId = identity.subject;
+  return userId as string;
+};
+
+export const archive = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await getUserIdentity(ctx);
+
+    const existingDocument = await ctx.db.get(args.id);
+    if (!existingDocument) throw new Error("Not available");
+
+    if (existingDocument.userId !== identity) throw new Error("Not authorized");
+
+    const dc = await ctx.db.patch(args.id, { isArchived: true });
+
+    const recursiveArchived = async (id: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (v) =>
+          v.eq("userId", args.id).eq("parentDocument", id)
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, { isArchived: true });
+      }
+    };
+
+    await recursiveArchived(args.id);
+    return dc;
+  },
+});
 
 export const getSidebar = query({
   args: { parentDocument: v.optional(v.id("documents")) },
