4f7068a28be91df86bf2fcc3c7fe729a94a51874
Milind Jamnekar <milindjamnekar502@gmail.com>
2024-02-15 18:54:33 +0530

    udpate mutation added

--- convex/documents.ts
+++ convex/documents.ts
@@ -229,3 +229,52 @@ export const getSearch = query({
     return documents;
   },
 });
+
+export const getById = query({
+  args: { documentId: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    const document = await ctx.db.get(args.documentId);
+
+    if (!identity) throw new Error("not athenticated");
+
+    if (!document) throw new Error("Not found");
+
+    if (document.isPublished && !document.isArchived) return document;
+
+    const userId = identity.subject;
+
+    if (document.userId !== userId) throw new Error("Not authorized");
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("documents"),
+    title: v.optional(v.string()),
+    content: v.optional(v.string()),
+    converImage: v.optional(v.string()),
+    icon: v.optional(v.string()),
+    isPublished: v.optional(v.boolean()),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) throw new Error("unauthenticated");
+
+    const userId = identity.subject;
+
+    const { id, ...rest } = args;
+
+    const existingDocument = await ctx.db.get(id);
+
+    if (!existingDocument) throw new Error("not found");
+
+    if (existingDocument.userId !== id) throw new Error("Not authorized");
+
+    const document = await ctx.db.patch(id, { ...rest });
+
+    return document;
+  },
+});
